6d322257edc90dfc06efc97d6a259a5bce06abf4
Teancum007 <icblamires@gmail.com>
2020-10-03 17:47:23 -0600

    Team activity 4

--- cse341-project-master/routes/ta03.js
+++ cse341-project-master/routes/ta03.js
@@ -2,13 +2,47 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    res.render('pages/ta03', { 
-        title: 'Team Activity 03', 
-        path: '/ta03', // For pug, EJS 
-        activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
+const fs = require('fs');
+const jdata = require('../models/items');
+
+router.get('/', (req, res, next) => {
+    const items = new jdata();
+    items.getItems((err, data) => {
+        if (err) {
+            console.log('File could not be read.');
+            res.end();
+        } else {
+            const itemsData = JSON.parse(data);
+            res.render('pages/ta03', { 
+                title: 'Team Activity 03', 
+                path: '/ta03', // For pug, EJS 
+                activeTA03: true, // For HBS
+                contentCSS: true, // For HBS
+                itemsData: itemsData
+            });
+        }
     });
 });
+
+router.post('/',(req, res, next) => {
+    const searchQuery = req.body.searchQuery;
+    const items = new jdata();
+    items.getItems((err, data) => {
+        if (err) {
+            console.log('File could not be read.');
+            res.end();
+        } else {
+            const itemsData = JSON.parse(data);
+            const foundItems = [];
+            let searchIndex = 0;
+
+            if (searchIndex >= 0) {
+                
+            }
+        }
+    });        
+});
+
+
 
 module.exports = router;
